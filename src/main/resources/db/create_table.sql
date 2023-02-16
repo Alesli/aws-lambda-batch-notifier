8c0f04f3c055b306a85244c451c59b22f6cad87e
Alesia_Skarakhod <Alesia.Skarakhod@wolterskluwer.com>
2023-02-16 04:33:04 +0200

    initial commit

--- src/main/resources/db/create_table.sql
+++
@@ -1,8 +0,0 @@
-create table image(
-    id               BIGINT          PRIMARY KEY AUTO_INCREMENT,
-    name             VARCHAR(20),
-    image_size       BIGINT,
-    file_extension   VARCHAR(40) ,
-    last_update_date       TIMESTAMP       DEFAULT NOW()
---    bitmap_id        VARCHAR(128)
-);
